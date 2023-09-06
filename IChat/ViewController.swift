468527a0205f7a279657dcc8c0b7e5388e851a44
Ilya L <cepctapk@gmail.com>
2023-09-06 19:24:19 +0300

    initial commit

--- IChat/ViewController.swift
+++ IChat/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.мшуц
+        view.backgroundColor = .cyan
     }
 
 
 }
 
+ 
